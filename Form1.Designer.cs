c148d09dc786d9646a390bde0238d05ab2764dbd
ddhello <sgfick@163.com>
2020-07-05 16:34:55 +0800

    1.1.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Wallpaper_Power
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(446, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "欢迎使用WallPaper Power 1.0.0(界面就先这样罢，懒得搞了)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "自动更换壁纸：";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(118, 60);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(277, 61);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "开机自动启动：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "壁纸数据源：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "二次元源1",
+            "二次元源2",
+            "二次元源3",
+            "二次元源4",
+            "二次元源5",
+            "二次元色图（慎用！）",
+            "360动漫壁纸",
+            "风景",
+            "妹子"});
+            this.comboBox1.Location = new System.Drawing.Point(106, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "自动更换间隔：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "秒";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 30);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(368, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 30);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "立即更换壁纸";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "WallPaper Power";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(323, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "程序是由萌萌哒的信息课代表写了200多行代码做出来的哦！";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(415, 62);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(320, 63);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 12);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "自动预载图片：";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(434, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 28);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "？";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 185);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "WallPaper Power 1.0.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,330 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Windows.Native;
+
+namespace Wallpaper_Power
+{
+    public partial class Form1 : Form
+    {
+        string url = "";
+        string er1 = "https://api.ixiaowai.cn/api/api.php";
+        string er2 = "http://api.btstu.cn/sjbz/?lx=dongman";
+        string er3 = "https://api.dujin.org/pic/";
+        string er4 = "https://api.dongmanxingkong.com/suijitupian/acg/1080p/index.php";
+        string er5 = "http://www.dmoe.cc/random.php";
+        string er6 = "https://api-misc.iclart.com/v1/acgapi";
+        string er7 = "https://api.isoyu.com/";
+        string er8 = "https://api.btstu.cn/doc/sjbz.php";
+        string er9 = "http://api.mtyqx.cn/";
+        string er10 = "https://api.yuzhitu.cn/";
+        string er11 = "https://api.yuzhitu.cn/sjbz/";
+        string fengjing = "https://api.ixiaowai.cn/gqapi/gqapi.php";
+        string meizi = "http://api.btstu.cn/sjbz/?lx=meizi";
+        string selected = "";
+        bool auto = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (ConfigurationManager.AppSettings["autochange"] == "True")
+            {
+                timer1.Enabled = true;
+                try
+                {
+                    timer1.Interval = Convert.ToInt16(ConfigurationManager.AppSettings["delay"]) * 1000;
+                    textBox1.Text = ConfigurationManager.AppSettings["delay"];
+                }
+                catch
+                {
+                    MessageBox.Show(this, "输入的时间间隔不合法！必须大于0！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            comboBox1.SelectedItem = ConfigurationManager.AppSettings["paper"];
+            if (ConfigurationManager.AppSettings["autorun"] == "True")
+            {
+                checkBox2.Checked = true;
+            }
+            else
+            {
+                checkBox2.Checked = false;
+            }
+            if (ConfigurationManager.AppSettings["autochange"] == "True")
+            {
+                checkBox1.Checked = true;
+            }
+            else checkBox1.Checked = false;
+            if (checkBox2.Checked == true)
+            {
+                bool isexc = false;
+                RegistryKey RKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                //设置自启的程序叫获取目录下的程序名字
+                string[] ar = RKey.GetValueNames();
+                foreach (string st in ar)
+                {
+                    if (st.Equals("WPP"))
+                    {
+                        isexc = true;
+                    }
+                }
+                if (!isexc)
+                {
+                    //设置自启的程序叫test
+                    RKey.SetValue("WPP", Application.StartupPath + @"\Wallpaper Power.exe");
+                }
+                RKey.Close();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == false)
+            {
+                string path = Application.ExecutablePath;
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+                try
+                {
+                    rk2.DeleteValue("WallPaper Power");
+                }
+                catch { }
+                rk2.Close();
+                rk.Close();
+            }
+            if (checkBox2.Checked == true)
+            {
+                UpdateAppSettings("autorun", "True");
+            }
+            else UpdateAppSettings("autorun", "False");
+        }
+        public static bool UpdateAppSettings(string key, string value)
+        {
+            var _config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (!_config.HasFile)
+            {
+                throw new ArgumentException("程序配置文件缺失！");
+            }
+            KeyValueConfigurationElement _key = _config.AppSettings.Settings[key];
+            if (_key == null)
+                _config.AppSettings.Settings.Add(key, value);
+            else
+                _config.AppSettings.Settings[key].Value = value;
+            _config.Save(ConfigurationSaveMode.Modified);
+            return true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                auto = false;
+            }
+            else auto = true;
+            if (checkBox1.Checked == true)
+            {
+                UpdateAppSettings("autochange", "True");
+            }
+            else UpdateAppSettings("autochange", "False");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateAppSettings("paper", comboBox1.SelectedItem.ToString());
+            if (comboBox1.SelectedItem.ToString() == "360动漫壁纸")
+            {
+                checkBox3.Enabled = true;
+
+            }
+            else checkBox3.Enabled = false;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            change();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateAppSettings("delay", textBox1.Text);
+            timer1.Enabled = true;
+            try
+            {
+                timer1.Interval = Convert.ToInt16(textBox1.Text) * 1000;
+            }
+            catch
+            {
+                MessageBox.Show(this, "输入的时间间隔不合法！必须大于0！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            change();
+        }
+        void change()
+        {
+            if (comboBox1.SelectedItem.ToString() == "360动漫壁纸")
+            {
+                string url = "http://wallpaper.apc.360.cn/index.php?c=WallPaperAndroid&a=getAppsByCategory&cid=26&start=0&count=99";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "HEAD";
+                request.AllowAutoRedirect = false;
+                HttpWebResponse myResp = (HttpWebResponse)request.GetResponse();
+                if (myResp.StatusCode == HttpStatusCode.Redirect)
+                { url = myResp.GetResponseHeader("Location"); }
+                WebClient webclient = new WebClient();
+                string json = webclient.DownloadString(url);
+                JObject jo = (JObject)JsonConvert.DeserializeObject(json);
+                string[] results = new string[99];
+                for (int a = 0; a <= 98; a++)
+                {
+                    results[a] = jo["Next"]["data"].ToString();
+                }
+
+            }
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "二次元源1":
+                    selected = er1;
+                    break;
+                case "二次元源2":
+                    selected = er2;
+                    break;
+                case "二次元源3":
+                    selected = er3;
+                    break;
+                case "二次元源4":
+                    selected = er4;
+                    break;
+                case "二次元源5":
+                    selected = er5;
+                    break;
+                case "二次元源6":
+                    selected = er6;
+                    break;
+                case "二次元源7":
+                    selected = er7;
+                    break;
+                case "二次元源8":
+                    selected = er8;
+                    break;
+                case "二次元源9":
+                    selected = er9;
+                    break;
+                case "二次元源10":
+                    selected = er10;
+                    break;
+                case "二次元源11":
+                    selected = er11;
+                    break;
+                case "二次元色图（慎用！）":
+                    MessageBox.Show("你在想屁吃");
+                    return;
+                    break;
+                case "风景":
+                    selected = fengjing;
+                    break;
+                case "妹子":
+                    selected = meizi;
+                    break;
+            }
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(selected);
+            req.Method = "HEAD";
+            req.AllowAutoRedirect = false;
+            try
+            {
+                HttpWebResponse myResp = (HttpWebResponse)req.GetResponse();
+                if (myResp.StatusCode == HttpStatusCode.Redirect)
+                { url = myResp.GetResponseHeader("Location"); }
+            }
+            catch
+            {
+                MessageBox.Show("使用该源获取壁纸失败，请尝试其他源");
+            }
+
+            WebClient web = new WebClient();
+            Random rd = new Random();
+            int i = rd.Next();
+            string pathp = Application.StartupPath + @"\img\" + i.ToString() + ".jpg";
+            try
+            {
+                web.DownloadFile(new Uri(url), pathp);
+            }
+            catch
+            {
+                MessageBox.Show("使用该源获取壁纸失败，请尝试其他源");
+            }
+            var ad = shlobj.GetActiveDesktop();
+            ad.SetWallpaper(pathp, 0);
+            ad.ApplyChanges(AD_Apply.ALL | AD_Apply.FORCE | AD_Apply.BUFFERED_REFRESH);
+            Marshal.ReleaseComObject(ad);
+        }
+
+        private void Form_SizeChanged(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.Show();
+            this.notifyIcon1.Visible = false;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {     //可以要，也可以不要，取决于是否隐藏主窗体
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "由于服务器限制，自动预载图片仅能在壁纸源选择360后使用\n注意：使用360壁纸动漫后可能会出现小猪佩奇等其他奇怪东西！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+    public class Data
+    {
+        public string pid { get; set; }
+        public string cid { get; set; }
+        public string dl_cnt { get; set; }
+        public string c_t { get; set; }
+        public string imgcut { get; set; }
+        public string url { get; set; }
+        public string tempdata { get; set; }
+        public string fav_total { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string errno { get; set; }
+        public string errmsg { get; set; }
+        public string consume { get; set; }
+        public string total { get; set; }
+        public List<Data> data { get; set; }
+    }
+}
